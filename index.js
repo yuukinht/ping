e6834b6c15d5666779faef53877b6b3c2c64bec6
yuukinht <50871323+yuukinht@users.noreply.github.com>
2020-01-15 21:42:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: process.env.PORT });
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+	ws.send(message);
+  });
+});
